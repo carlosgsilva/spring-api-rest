c13f927f6dccfb67589c301ac820dca01aed509b
carlosgsilva <carlosgsilva.dev@gmail.com>
2021-07-16 22:32:41 -0400

    Spring Initializr

---
+++ src/main/java/br/com/application/api/SpringApiRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.application.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/application/api/SpringApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.application.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
