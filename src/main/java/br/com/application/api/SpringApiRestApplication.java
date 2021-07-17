bfaf19dbe26af8a086a4ff67feb8cdbd8c716a31
carlosgsilva <carlosgsilva.dev@gmail.com>
2021-07-16 23:07:37 -0400

    Mapping Creation 'Mapper'

--- src/main/java/br/com/application/api/SpringApiRestApplication.java
+++ src/main/java/br/com/application/api/SpringApiRestApplication.java
@@ -1,13 +1,24 @@
 package br.com.application.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringApiRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiRestApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+
+		modelMapper.getConfiguration().setAmbiguityIgnored(true);
+
+		return modelMapper;
 	}
 
 }
