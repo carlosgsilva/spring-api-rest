921ff02752183821e2788f673aeafe03bb5e24ea
carlosgsilva <carlosgsilva.dev@gmail.com>
2021-07-16 22:52:24 -0400

    Create Class UserRepository

---
+++ src/main/java/br/com/application/api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.application.api.repository;
+
+import br.com.application.api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
