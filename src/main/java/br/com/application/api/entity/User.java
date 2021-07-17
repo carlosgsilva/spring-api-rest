cb042fa2d9436fd150f175f20719f9709192c6da
carlosgsilva <carlosgsilva.dev@gmail.com>
2021-07-16 22:47:18 -0400

    Creat User Class

---
+++ src/main/java/br/com/application/api/entity/User.java
@@ -0,0 +1,27 @@
+package br.com.application.api.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name = "users")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column
+    private String name;
+    @Column
+    private String login;
+    @Column
+    private String password;
+    
+}
